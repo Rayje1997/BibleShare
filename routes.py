6184508634c447f634aab19fdd64b9453d83b5e8
Rayje1997 <rayje1997@gmail.com>
2018-11-16 15:38:36 -0500

    added the ability to read in .json files as dictionarys.

--- routes.py
+++ routes.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, url_for
+import json
+
+# loads the Bible from KJV.json into a dictionary
+with open('static/js/KJV.json') as b:
+    bible = json.load(b)
+
+# loads users.json into a dictionary
+with open('static/js/users.json') as u:
+    users = json.load(u)
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', users=users)
 
 @app.route('/home')
 def home():
     return render_template('home.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug = True)
