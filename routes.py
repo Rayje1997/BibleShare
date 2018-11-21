36b80a2eb9322f1f9a0a0f5572be65a6eef579b7
Jessie Ray <30801211+Rayje1997@users.noreply.github.com>
2018-11-21 15:11:48 -0500

    Imported session and removed some of the test code

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session, request
 import json
 
 # loads the Bible from KJV.json into a dictionary
