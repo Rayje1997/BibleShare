43fa99f02e651e482a079e1648510310006da9cf
Rayje1997 <rayje1997@gmail.com>
2018-11-22 20:09:09 -0500

    added a function/page to handle login requests. This needs to fleshed out and integrated with the form that was created to submit the information. This will need to compare the input to the users.json file to determine if the login was legitimate then create a user session.

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, session, request
 import json
+import random
 
 # loads the Bible from KJV.json into a dictionary
 with open('static/js/KJV.json') as b:
@@ -12,13 +13,23 @@ with open('static/js/users.json') as u:
 app = Flask(__name__)
 app.static_folder = 'static'
 
-@app.route('/')
+app.secret_key = random.randrange(1000000)
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
+    """Controller function for the login page"""
     return render_template('index.html', users=users)
 
 @app.route('/home')
 def home():
+    """Controller function for the home page"""
     return render_template('home.html', bible = bible)
+
+@app.route('/checkLogin', methods=['GET', 'POST'])
+def checkLogin():
+    """Controller function for the checkLogin page"""
+    return render_template('checkLogin.html', userData = request.data)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
