3e9d661794859f9ba4b93f2c3bbc7b9c9c80d5bd
Rayje1997 <rayje1997@gmail.com>
2018-11-23 11:39:50 -0500

    Added some login functionality. It currently only checks if the username exists and then creates a session with the username, but it should be simple to add password checking to it later. Now that the login page works I can begin working on redering the Bible properly.

--- routes.py
+++ routes.py
@@ -1,34 +1,50 @@
-from flask import Flask, render_template, url_for, session, request
+from flask import Flask, render_template, url_for, session, request, redirect
 import json
 import random
+
+#######################################
+# initial variable setup
+######################################
 
 # loads the Bible from KJV.json into a dictionary
 with open('static/js/KJV.json') as b:
     bible = json.load(b)
-
 # loads users.json into a dictionary
 with open('static/js/users.json') as u:
     users = json.load(u)
-
 app = Flask(__name__)
 app.static_folder = 'static'
+app.secret_key = str(random.randrange(1000000))
 
-app.secret_key = random.randrange(1000000)
+########################################
+# end of variable setup
+#######################################
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     """Controller function for the login page"""
-    return render_template('index.html', users=users)
+
+    return render_template('index.html')
 
 @app.route('/home')
 def home():
     """Controller function for the home page"""
     return render_template('home.html', bible = bible)
 
-@app.route('/checkLogin', methods=['GET', 'POST'])
+@app.route('/checkLogin', methods = ['GET', 'POST'])
 def checkLogin():
-    """Controller function for the checkLogin page"""
-    return render_template('checkLogin.html', userData = request.data)
+
+    error = None
+
+    if request.method == 'POST':
+        for user in range(len(users)):
+            if request.form['username'] == users[user]['username']:
+                session['username'] = request.form['username']
+                return redirect(url_for('home'))
+            else:
+                error = "Invalid login credentials. Please login."
+                return redirect(url_for('index'))
+    return render_template('checkLogin.html')
 
 
 if __name__ == '__main__':
